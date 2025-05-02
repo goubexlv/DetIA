d58620e69764d9ca8e512772186b5f6bb1bff85f
goubexlv <ngoubenejunior@gmail.com>
2025-05-02 10:42:07 +0000

    creation du projet DetIA

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "📥 Téléchargement ou chargement du modèle..."
-/app/venv/bin/python /app/telemodel.py
+/app/venv/bin/python3 /app/telemodel.py
 
 echo "🚀 Lancement de l'application Ktor..."
 exec java -jar /app/Detia.jar
